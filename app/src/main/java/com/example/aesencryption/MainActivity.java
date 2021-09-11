5fd304d316d3d0757e33a9177d3be239500f1253
Abhinav82193 <abhinavkhare786.ak@gmail.com>
2021-09-11 12:01:23 +0530

    change in copy function

--- app/src/main/java/com/example/aesencryption/MainActivity.java
+++ app/src/main/java/com/example/aesencryption/MainActivity.java
@@ -24,12 +24,14 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     EditText et_key;
-    EditText et_message,message;
+    EditText et_message;
+    TextView message;
     ImageButton b1,b2;
     String encrpyted,pastedata;
     ClipboardManager clipboardmanager;
     ClipData clipdata;
     String inputkey,inputmessage;
+
 
 
     @Override
@@ -44,12 +46,20 @@ public class MainActivity extends AppCompatActivity {
       ClipboardManager clipboardManager = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
 
       //  b2.setEnabled(false);
+       if(message.length()==0) {
+//            b1.setEnabled(false);
+           b1.getVisibility();
+           }
 
-    }
+       }
+
+
+
 
 
 
     public void encrypt(View view) throws GeneralSecurityException {
+        b1.setEnabled(true);
         if(et_key.length()>0&&et_message.length()>0){
              inputkey=et_key.getText().toString();
              inputmessage=et_message.getText().toString();
@@ -57,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
          et_message.setText("");
          et_key.setText("");
          message.setText(String.format("%s", encrpyted));
+         b1.getVisibility();
+       //  b1.setEnabled(true);
 
 
      }catch (GeneralSecurityException e){
@@ -68,24 +80,35 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void copy(View view) {
-        ClipboardManager clipboardManager = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
-        ClipData clipData = ClipData.newPlainText("lable", encrpyted);
-        clipboardManager.setPrimaryClip(clipData);
-        Toast.makeText(this, "Your Message Was Copied to clip board", Toast.LENGTH_SHORT).show();
+        String encrypted = message.getText().toString();
+        if(encrypted.length()>0) {
 
+            ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clipData = ClipData.newPlainText("lable", encrpyted);
+            clipboardManager.setPrimaryClip(clipData);
+            Toast.makeText(this, "Your Message Was Copied to clip board", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "Please Create a Encrypted message", Toast.LENGTH_SHORT).show();
+        }
     }
 
 
     public void paste(View view) {
 
-        ClipboardManager clipboardManager = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
+        try {  ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData.Item item = clipboardManager.getPrimaryClip().getItemAt(0);
+            String pastedata = item.getText().toString();
+            Toast.makeText(this, "Your Message Was Pasted to clip board", Toast.LENGTH_SHORT).show();
+            et_message.setText(pastedata);}
+        catch(Exception  e){
+            e.printStackTrace();
+        }
 
-        ClipData.Item item= clipboardManager.getPrimaryClip().getItemAt(0);
-        String pastedata=item.getText().toString();
-        Toast.makeText(this, "Your Message Was Pasted to clip board", Toast.LENGTH_SHORT).show();
-        et_message.setText(pastedata);
+        }
 
-    }
+
+
 
 
 
