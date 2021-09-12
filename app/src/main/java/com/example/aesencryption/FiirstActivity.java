ef58f97a84c85728c6815c58594fd57171da1170
Abhinav82193 <abhinavkhare786.ak@gmail.com>
2021-09-12 13:41:06 +0530

    decrypt function 23

---
+++ app/src/main/java/com/example/aesencryption/FiirstActivity.java
@@ -0,0 +1,14 @@
+package com.example.aesencryption;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FiirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fiirst);
+    }
+}
